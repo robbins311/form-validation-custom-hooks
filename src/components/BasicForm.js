8454774606d552427774aea93b8317db6f6ad27e
robbins312 <robbins312@naver.com>
2023-11-28 16:50:08 +0900

    add customhooks

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,22 +1,98 @@
+import useBasic from "../hooks/use-basic";
+
+const isNotEmpty = (value) => value.trim() !== "";
+const isEmail = (value) => value.includes("@");
 const BasicForm = (props) => {
+  const {
+    value: enteredFirstName,
+    isValid: enteredFirstNameIsValid,
+    hasError: firstHasError,
+    valueChangeHandler: firstnameChangeHandler,
+    inputBlurHandler: firstBlurHandler,
+    reset: firstReset,
+  } = useBasic(isNotEmpty);
+  const {
+    value: enteredLastName,
+    isValid: enteredLastNameIsValid,
+    hasError: lastHasError,
+    valueChangeHandler: lastnameChangeHandler,
+    inputBlurHandler: lastBlurHandler,
+    reset: lastReset,
+  } = useBasic(isNotEmpty);
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: emailReset,
+  } = useBasic(isEmail);
+
+  let formIsValid = false;
+
+  if (
+    enteredFirstNameIsValid &&
+    enteredLastNameIsValid &&
+    enteredEmailIsValid
+  ) {
+    formIsValid = true;
+  }
+
+  const submissionForm = (event) => {
+    event.preventDefault();
+    if (!enteredFirstNameIsValid) {
+      return;
+    }
+    firstReset();
+    lastReset();
+    emailReset();
+  };
+  const firstClasses = firstHasError ? "form-control invalid" : "form-control";
+  const lastClasses = lastHasError ? "form-control invalid" : "form-control";
+  const emailClasses = emailHasError ? "form-control invalid" : "form-control";
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={submissionForm}>
+      <div className="control-group">
+        <div className={firstClasses}>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            value={enteredFirstName}
+            onChange={firstnameChangeHandler}
+            onBlur={firstBlurHandler}
+            type="text"
+            id="firstName"
+          />
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        {firstHasError && (
+          <p className="error-text">FirstName must not be empty</p>
+        )}
+        <div className={lastClasses}>
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            value={enteredLastName}
+            onChange={lastnameChangeHandler}
+            onBlur={lastBlurHandler}
+            type="text"
+            id="lastName"
+          />
         </div>
+        {lastHasError && (
+          <p className="error-text">LastName must not be empty</p>
+        )}
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={emailClasses}>
+        <label htmlFor="email">E-Mail Address</label>
+        <input
+          value={enteredEmail}
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          type="email"
+          id="email"
+        />
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      {emailHasError && <p className="error-text">enter valid email</p>}
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
--- src/hooks/use-basic.js
+++ src/hooks/use-basic.js
@@ -1,2 +1,29 @@
-const useBasic = () => {};
+import { useState } from "react";
+
+const useBasic = (validateValue) => {
+  const [value, setValue] = useState("");
+  const [isTouched, setIsTouced] = useState(false);
+
+  const valueIsValid = validateValue(value);
+  const hasError = !valueIsValid && isTouched;
+
+  const valueChangeHandler = (e) => {
+    setValue(e.target.value);
+  };
+  const inputBlurHandler = () => {
+    setIsTouced(true);
+  };
+  const reset = () => {
+    setValue("");
+    setIsTouced(false);
+  };
+  return {
+    value,
+    isValid: valueIsValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
+};
 export default useBasic;
